6a8f167e4c1e5c26d249032946bf62fc4bffc047
Jeremy Woertink <jeremywoertink@gmail.com>
2023-01-19 11:31:30 -0800

    removed more things not necessary

--- src/actions/home/index.cr
+++ src/actions/home/index.cr
@@ -1,18 +1,13 @@
 class CustomTextResponse < Lucky::Response
-  getter context, content_type, body, enable_cookies
+  getter context, content_type, body
 
   def initialize(@context : HTTP::Server::Context,
                  @content_type : String,
-                 @body : String | IO,
-                 @enable_cookies : Bool = true)
+                 @body : String | IO
+                )
   end
 
   def print : Nil
-    # if enable_cookies
-    #   write_flash
-    #   write_session
-    #   write_cookies
-    # end
     context.response.content_type = content_type
     context.response.status_code = status
     context.response.print(body)
@@ -25,34 +20,9 @@ class CustomTextResponse < Lucky::Response
   def debug_message : String?
     nil
   end
-
-  private def write_flash : Nil
-    context.session.set(
-      Lucky::FlashStore::SESSION_KEY,
-      context.flash.to_json
-    )
-  end
-
-  private def write_session : Nil
-    context.cookies.set(
-      Lucky::Session.settings.key,
-      context.session.to_json
-    )
-  end
-
-  private def write_cookies : Nil
-    response = context.response
-
-    context.cookies.updated.each do |cookie|
-      response.cookies[cookie.name] = cookie
-    end
-
-    response.cookies.add_response_headers(response.headers)
-  end
 end
 
 class Home::Index < Lucky::Action
-  disable_cookies
   accepted_formats [:json]
 
   get "/" do
@@ -63,7 +33,6 @@ class Home::Index < Lucky::Action
       context,
       "application/json",
       {data: data}.to_json,
-      enable_cookies: false
     )
   end
 end
