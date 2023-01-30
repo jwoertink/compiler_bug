3d18bb2ee899acd03f5c0b54cba94378faea2901
Jeremy Woertink <jeremywoertink@gmail.com>
2023-01-29 16:16:44 -0800

    less crystal code

--- src/actions/home/index.cr
+++ src/actions/home/index.cr
@@ -1,38 +1,9 @@
-class CustomTextResponse < Lucky::Response
-  getter context, content_type, body
-
-  def initialize(@context : HTTP::Server::Context,
-                 @content_type : String,
-                 @body : String | IO
-                )
-  end
-
-  def print : Nil
-    context.response.content_type = content_type
-    context.response.status_code = status
-    context.response.print(body)
-  end
-
-  def status : Int
-    200
-  end
-
-  def debug_message : String?
-    nil
-  end
-end
-
 class Home::Index < Lucky::Action
   accepted_formats [:json]
 
   get "/" do
     uri = "https://api.punkapi.com/v2/beers"
     response = HTTP::Client.exec("GET", uri)
-    data = JSON.parse(response.body)
-    CustomTextResponse.new(
-      context,
-      "application/json",
-      {data: data}.to_json,
-    )
+    raw_json(response.body.to_s)
   end
 end
